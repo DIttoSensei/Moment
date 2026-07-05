c5a4fff49f758cb9760e2629e8eb090ac227acc0
DIttoSensei <104984682+DIttoSensei@users.noreply.github.com>
2026-07-05 09:09:32 +0100

    vote fix

--- api/vote.js
+++ api/vote.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
-  const url = process.env.UPSTASH_REDIS_REST_URL;
-  const token = process.env.UPSTASH_REDIS_REST_TOKEN;
+  const url = process.env.KV_REST_API_URL;
+  const token = process.env.KV_REST_API_TOKEN;
 
   if (req.method === 'GET') {
     const response = await fetch(`${url}/get/votes`, {
